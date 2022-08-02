27ce46bb8a4ed1d68300f7610d7e28b4f0148f5b
pedrohbsilva <phbs1235@gmail.com>
2022-08-01 21:04:43 -0300

    atualização para db postgres

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,7 +2,21 @@ import os
 from flask import Flask, jsonify
 from src.app.config import app_config
 from src.app.routes import routes
-app = Flask(__name__)
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-app.config.from_object(app_config[os.getenv('FLASK_ENV')])
-routes(app)
+db = SQLAlchemy()
+ma = Marshmallow()
+
+def create_app():
+
+  app = Flask(__name__)
+
+  app.config.from_object(app_config[os.getenv('FLASK_ENV')])
+  db.init_app(app)
+  ma.init_app(app)
+  routes(app)
+  Migrate(app=app, db=db, directory='./src/app/migrations')
+
+  return app
--- src/app/config/__init__.py
+++ src/app/config/__init__.py
@@ -7,9 +7,13 @@ load_dotenv(find_dotenv())
 class Development(object):
   DEBUG = True
   TESTING = False
+  SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+  SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 
 class Production(object):
   DEBUG = False
   TESTING = False
+  SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+  SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 
 app_config = {"development": Development, "production": Production}
